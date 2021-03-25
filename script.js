17e7977f623722769cedf5d76ba678448c77701d
victorphillips1 <victor.phillips@ocsbstudent.ca>
2021-03-25 14:39:13 +0000

    made the number random

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
+// random number generator
+rval = Math.floor(Math.random() * 100)
+rval = rval + 1
 // loop
-while(uimp !== 15) {
- var uimp = prompt('guess my age!')
+while(uimp !== rval) {
+ var uimp = prompt('guess a number between 1 and 100')
  uimp = parseInt(uimp)
- if (uimp == 15) {
+ if (uimp == rval) {
    alert('congrats, you guessed corectly!')
- } else if (uimp > 15) {
+ } else if (uimp > rval) {
    alert('your guess is too high')
  } else {
    alert('your guess was too low')
