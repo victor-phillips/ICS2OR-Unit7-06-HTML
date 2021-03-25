91b89758395b0328d8eb47a09d61794d077c3025
victorphillips1 <victor.phillips@ocsbstudent.ca>
2021-03-25 14:48:55 +0000

    fixed stuff

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 // random number generator
-rval = Math.floor(Math.random() * 100)
+let rval = Math.floor(Math.random() * 100)
 rval = rval + 1
+let uimp = 0
 // loop
-while(uimp !== rval) {
- var uimp = prompt('guess a number between 1 and 100')
- uimp = parseInt(uimp)
- if (uimp == rval) {
-   alert('congrats, you guessed corectly!')
- } else if (uimp > rval) {
-   alert('your guess is too high')
- } else {
-   alert('your guess was too low')
+ while (uimp !== rval) {
+  uimp = prompt('guess a number between 1 and 100')
+  uimp = parseInt(uimp)
+  if (uimp === rval) {
+    alert('congrats, you guessed corectly!')
+  } else if (uimp > rval) {
+    alert('your guess is too high')
+  } else {
+    alert('your guess was too low')
  }
 }
